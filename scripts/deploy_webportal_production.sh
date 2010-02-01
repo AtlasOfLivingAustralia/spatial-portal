f54ea0348ec14832a27522da98b650965ee760e8
brendon.ward@sixlighthouses.com <brendon.ward@sixlighthouses.com>
2010-02-01 09:57:31 +0000

    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@9 73def43e-0e3b-11df-a5af-b38b39d06e06

--- scripts/deploy_webportal_production.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-SCRIPT_DIR=`dirname $0`
-BACKUP_DIR="backup"
-WARFILE_BASENAME="webportal"
-TOMCAT_PATH="/usr/local/tomcat/instance_02_webportal"
-SERVER="imos.aodn.org.au"
-
-. $SCRIPT_DIR/deploy_warfile.sh
--- trunk/src/main/java/au/org/emii/portal/ServiceProcessor.java
+++
@@ -1,6 +0,0 @@
-package au.org.emii.portal;
-import au.org.emii.portal.config.xmlbeans.Service;
-
-public interface ServiceProcessor {
-	public MapLayer service(Service service);
-}
