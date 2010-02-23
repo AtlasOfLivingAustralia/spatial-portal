46212900fe0c2c9902171ee2af8a378c25898d4f
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-02-23 08:17:42 +0000

    Initial version of gazetteer-search geoserver plugin.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@38 73def43e-0e3b-11df-a5af-b38b39d06e06

---
+++ gazetteer-search/src/main/java/org/ala/rest/GazetteerSearch.java
@@ -0,0 +1,15 @@
+package org.ala.rest;
+
+
+public class GazetteerSearch {
+
+   String message;
+
+   public GazetteerSearch( String message ) {
+       this.message = message;
+   }
+
+   public String getMessage() {
+      return message;
+   }
+}
