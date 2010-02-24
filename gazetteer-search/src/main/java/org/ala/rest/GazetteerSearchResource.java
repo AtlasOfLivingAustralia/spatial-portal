9372207e7418c74d6b7c46db1dbce65ffb8f1b03
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-02-24 23:20:49 +0000

    Tidied code. Fixed test cases.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@41 73def43e-0e3b-11df-a5af-b38b39d06e06

--- gazetteer-search/src/main/java/org/ala/rest/GazetteerSearchResource.java
+++ gazetteer-search/src/main/java/org/ala/rest/GazetteerSearchResource.java
@@ -6,10 +6,10 @@ public class GazetteerSearchResource extends ReflectiveResource {
    
    @Override
    protected Object handleObjectGet() throws Exception {
-       //Map<String, Object> map = getRequest().getAttributes().get("q");
-       String stringValue = getRequest().getAttributes().get("q").toString().split("=")[1]; //map.get("q");
-       System.out.println(stringValue);
+       //hmmm the getAttributes stuff doesn't seem to work as documented 
+       String stringValue = getRequest().getAttributes().get("q").toString().split("=")[1]; 
+       //System.out.println(stringValue);
        
        return new GazetteerSearch(stringValue);
-	}
+   }
 }
