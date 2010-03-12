ed62d0df5d6ddc3915215bc7f85eeae909f5b6af
brendon.ward@sixlighthouses.com <brendon.ward@sixlighthouses.com>
2010-03-12 20:47:09 +0000

    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@51 73def43e-0e3b-11df-a5af-b38b39d06e06

--- webportal/src/main/java/au/org/emii/portal/wms/BaseLayerProcessor.java
+++
@@ -1,8 +0,0 @@
-package au.org.emii.portal.wms;
-
-import au.org.emii.portal.menu.MapLayer;
-import au.org.emii.portal.config.xmlbeans.BaseLayer;
-
-public interface BaseLayerProcessor {
-	public MapLayer baseLayer(BaseLayer baseLayer);
-}
