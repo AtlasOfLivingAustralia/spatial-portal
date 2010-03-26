bafe45cd36df7defdef38ad471bcac6243988261
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-03-26 03:12:04 +0000

    Test cases working again, uses a copy of the gazetteer index
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@63 73def43e-0e3b-11df-a5af-b38b39d06e06

--- gazetteer-search/src/test/java/org/ala/rest/GazetteerSearchResourceTest.java
+++ gazetteer-search/src/test/java/org/ala/rest/GazetteerSearchResourceTest.java
@@ -5,44 +5,45 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import net.sf.json.JSON;
 import net.sf.json.JSONObject;
+import java.io.File;
+import org.vfny.geoserver.global.GeoserverDataDirectory;
+import org.apache.commons.io.FileUtils;
 
 public class GazetteerSearchResourceTest extends GeoServerTestSupport {
 
-     public void testGetAsXML() throws Exception {
+     
+    public void testGetAsXML() throws Exception {
+     //Copy the lucene index into the mock data directory (assumes test-index is in target)
+     FileUtils.copyDirectoryToDirectory(new File(GeoserverDataDirectory.getGeoserverDataDirectory().getParent(),"test-index"), GeoserverDataDirectory.getGeoserverDataDirectory());
+
      //make the request, parsing the result as a dom
-   /* Document dom = getAsDOM( "/rest/gazetteer-search/result.xml?q=cheese" );
+     Document dom = getAsDOM( "/rest/gazetteer-search/result.xml?q=Australia" );
 
      //print out the result
-     print(dom);
+    // print(dom);
 
      //make assertions
      Node message = getFirstElementByTagName( dom, "name");
-    assertNotNull(message);
-     assertEquals( "Some cheese place", message.getFirstChild().getNodeValue() );
-     */
-	
-     assertTrue(1==1);
+     assertNotNull(message);
+     assertEquals( "australia", message.getFirstChild().getNodeValue() );
    }
 
     public void testGetAsJSON() throws Exception {
+     //Copy the lucene index into the mock data directory (assumes test-index is in target)
+     FileUtils.copyDirectoryToDirectory(new File(GeoserverDataDirectory.getGeoserverDataDirectory().getParent(),"test-index"), GeoserverDataDirectory.getGeoserverDataDirectory());
+   
      //make the request, parsing the result into a json object
-  /*   JSON json = getAsJSON( "/rest/gazetteer-search/result.json?q=Australia");
+     JSON json = getAsJSON( "/rest/gazetteer-search/result.json?q=Australia");
    
      //print out the result
      print(json);
 
      //make assertions
-    assertTrue( json instanceof JSONObject );
+     assertTrue( json instanceof JSONObject );
      JSONObject search = ((JSONObject) json).getJSONObject( "org.ala.rest.GazetteerSearch" );
-     String name = ((JSONObject)search.getJSONObject("results").getJSONArray("org.ala.rest.SearchResultItem").get(0)).get("name").toString();
-     System.out.println(name);
-     assertEquals( "Australia", name ); */
-     assertTrue(1==1);
+     String result = (String)((JSONObject)(search.getJSONObject("results").getJSONArray("org.ala.rest.SearchResultItem").get(0))).get("name");
+     assertEquals( "australia", result ); 
    
    } 
     
-   /* public void testGetAsText() throws Exception {
-	//assertEquals( "q=blah", getAsString("/rest/gazetteer-search/result.txt?q=blah"));
-	assertTrue(1==1);
-    }*/
 }
