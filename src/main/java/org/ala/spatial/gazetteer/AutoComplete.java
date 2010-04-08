9c379b5a994cdda15d4bfe21c6a37771a643f342
brendon.ward@sixlighthouses.com <brendon.ward@sixlighthouses.com>
2010-04-08 04:26:04 +0000

    updates for dynamic resizing of analysis tab
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/webportal@87 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/spatial/gazetteer/AutoComplete.java
+++ src/main/java/org/ala/spatial/gazetteer/AutoComplete.java
@@ -12,7 +12,7 @@ import org.apache.http.impl.client.*;
 import org.apache.http.impl.auth.*;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.*; 
+import org.apache.http.client.*;
 import org.apache.http.client.methods.*;
 import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.HttpHost;
@@ -20,99 +20,97 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.util.EntityUtils;
-import org.apache.http.impl.client.BasicCredentialsProvider; 
+import org.apache.http.impl.client.BasicCredentialsProvider;
 
 public class AutoComplete extends Combobox {
-      public AutoComplete() {
+
+    public AutoComplete() {
         refresh(""); //init the child comboitems
-      }
-      public AutoComplete(String value) {
+    }
+
+    public AutoComplete(String value) {
         super(value); //it invokes setValue(), which inits the child comboitems
-      }
- 
-      public void setValue(String value) {
+    }
+
+    public void setValue(String value) {
         super.setValue(value);
         refresh(value); //refresh the child comboitems
-      }
-      /** Listens what an user is entering.
-       */
-      public void onChanging(InputEvent evt) {
+    }
+
+    /** Listens what an user is entering.
+     */
+    public void onChanging(InputEvent evt) {
         refresh(evt.getValue());
-      }
- 
-      /** Refresh comboitem based on the specified value.
-       */
-      private void refresh(String val) {
-	//update the dictionary
-		//TODO: remove hardcoded host, credentials
- 		HttpHost targetHost = new HttpHost("ec2-184-73-34-104.compute-1.amazonaws.com", 80, "http");
- 		DefaultHttpClient httpclient = new DefaultHttpClient();
-		// Add AuthCache to the execution context
-		BasicHttpContext localcontext = new BasicHttpContext();
-		//localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);    		
-		String searchString = val.trim().replaceAll("\\s+","+");	
-		HttpGet httpget = new HttpGet(
-			     "/geoserver/rest/gazetteer-search/result.json?q=" + searchString);
-	
+    }
 
-		try
-		{
-		    HttpResponse response = httpclient.execute(targetHost, httpget, localcontext);
-		    HttpEntity entity = response.getEntity();
-		    String responseText = "";
-		    if (entity != null) {
-            		responseText = new String(EntityUtils.toByteArray(entity));
-		        } else {
-		           responseText = "Fail";
-		        }
-		//resultLabel.setValue(responseText);
-		 		  JSONObject responseJson = JSONObject.fromObject(responseText);		  
-		  JSONObject search = responseJson.getJSONObject( "org.ala.rest.GazetteerSearch" );
-		  JSONArray results = search.getJSONObject("results").getJSONArray("org.ala.rest.SearchResultItem");
-		  //_dict = new String[results.size()];
-		  Iterator it = getItems().iterator();
+    /** Refresh comboitem based on the specified value.
+     */
+    private void refresh(String val) {
+        //update the dictionary
+        //TODO: remove hardcoded host, credentials
+        HttpHost targetHost = new HttpHost("ec2-184-73-34-104.compute-1.amazonaws.com", 80, "http");
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        // Add AuthCache to the execution context
+        BasicHttpContext localcontext = new BasicHttpContext();
+        //localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
+        String searchString = val.trim().replaceAll("\\s+", "+");
+        HttpGet httpget = new HttpGet(
+                "/geoserver/rest/gazetteer-search/result.json?q=" + searchString);
 
-		  for (int i = 0; i<results.size();i++){ 
-			 String itemString = (String)results.getJSONObject(i).get("name");
-			 if (it != null && it.hasNext()) {
-           		 ((Comboitem)it.next()).setLabel(itemString);
-          			} else {
-           		 it = null;
-           		 new Comboitem(itemString).setParent(this);
-          		}
 
-			}
-		    while (it != null && it.hasNext()) {
-         	 	it.next();
-         		 it.remove();
-			}
+        try {
+            HttpResponse response = httpclient.execute(targetHost, httpget, localcontext);
+            HttpEntity entity = response.getEntity();
+            String responseText = "";
+            if (entity != null) {
+                responseText = new String(EntityUtils.toByteArray(entity));
+            } else {
+                responseText = "Fail";
+            }
+            //resultLabel.setValue(responseText);
+            JSONObject responseJson = JSONObject.fromObject(responseText);
+            JSONObject search = responseJson.getJSONObject("org.ala.rest.GazetteerSearch");
+            JSONArray results = search.getJSONObject("results").getJSONArray("org.ala.rest.SearchResultItem");
+            //_dict = new String[results.size()];
+            Iterator it = getItems().iterator();
 
-		}
-		catch(Exception e)
-		{
-			
-		}
+            for (int i = 0; i < results.size(); i++) {
+                String itemString = (String) results.getJSONObject(i).get("name");
+                if (it != null && it.hasNext()) {
+                    ((Comboitem) it.next()).setLabel(itemString);
+                } else {
+                    it = null;
+                    new Comboitem(itemString).setParent(this);
+                }
 
-	//        
+            }
+            while (it != null && it.hasNext()) {
+                it.next();
+                it.remove();
+            }
 
-	/*int j = Arrays.binarySearch(_dict, val);
+        } catch (Exception e) {
+        }
+
+        //
+
+        /*int j = Arrays.binarySearch(_dict, val);
         if (j < 0) j = -j-1;
- 
+
         Iterator it = getItems().iterator();
         for (int cnt = 10; --cnt >= 0 && j < _dict.length && _dict[j].startsWith(val); ++j) {
-          if (it != null && it.hasNext()) {
-            ((Comboitem)it.next()).setLabel(_dict[j]);
-          } else {
-            it = null;
-            new Comboitem(_dict[j]).setParent(this);
-          }
+        if (it != null && it.hasNext()) {
+        ((Comboitem)it.next()).setLabel(_dict[j]);
+        } else {
+        it = null;
+        new Comboitem(_dict[j]).setParent(this);
         }
- 
+        }
+
         while (it != null && it.hasNext()) {
-          it.next();
-          it.remove();
+        it.next();
+        it.remove();
         }*/
-      }
- 
-    //  private static String[] _dict = { ""};
     }
+    //  private static String[] _dict = { ""};
+}
