e75110803e0102ab57b03aea4bdf387fffc9e1e5
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-04-15 13:40:39 +0000

    Connects to database features now, uses a config class to figure out which attribute to query on. Config is just hardcoded at the moment.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/gazetteer-search@102 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/rest/GazetteerFeature.java
+++ src/main/java/org/ala/rest/GazetteerFeature.java
@@ -2,12 +2,17 @@ package org.ala.rest;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 
 import java.util.Map;
+import javax.servlet.ServletContext;
+
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.data.CatalogReader;
@@ -15,13 +20,18 @@ import org.geotools.data.FeatureSource;
 import org.geotools.data.Query;
 import org.geotools.data.postgis.PostgisNGDataStoreFactory;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
 import org.opengis.feature.Feature;
 
 import org.geoserver.config.GeoServer;
 
 import org.geoserver.config.impl.GeoServerImpl;
 import org.geoserver.platform.GeoServerExtensions;
+import org.geotools.filter.text.cql2.CQL;
+import org.opengis.feature.Property;
 import org.vfny.geoserver.global.GeoserverDataDirectory;
+
+import org.vfny.geoserver.util.DataStoreUtils;
 
 /**
  *
@@ -30,66 +40,50 @@ import org.vfny.geoserver.global.GeoserverDataDirectory;
 public class GazetteerFeature {
 
 
-    Object theFeature;
+    String name;
+//    List properties;
 
     public GazetteerFeature(String layerName, String featureName) throws IOException, Exception {
 
-    GeoServer gs = GeoServerExtensions.bean( GeoServer.class );
+        //Get the gazetteer config - //TODO: should make this a spring singleton
+        GazetteerConfig gc = new GazetteerConfig(new File(GeoserverDataDirectory.getGeoserverDataDirectory(),"gazetteer.xml"));
 
-    Catalog catalog = gs.getCatalog();
+        GeoServer gs = GeoServerExtensions.bean( GeoServer.class );
+        ServletContext sc = GeoServerExtensions.bean( ServletContext.class );
+        Catalog catalog = gs.getCatalog();
 
-    LayerInfo layerInfo = catalog.getLayerByName(layerName);
-    Map params = layerInfo.getResource().getStore().getConnectionParameters(); //.getName().toString();
-    // File catalog = new File(GeoserverDataDirectory.getGeoserverDataDirectory(),"catalog.xml");
-      // File catalog = new File("/media/raid-storage/ALA Data/ALA - Dave Martin Data/catalog.xml");
-//        CatalogReader reader = new CatalogReader();
-//                  reader.read( catalog );
-//                  List dataStores = reader.dataStores();
-//                  Map namespaces = reader.namespaces();
-//               System.out.println("****" + dataStores.size());
-                  //   Map dataStoreParams = (Map)dataStores.get(0);
+        LayerInfo layerInfo = catalog.getLayerByName(layerName);
+        Map params = layerInfo.getResource().getStore().getConnectionParameters();
 
+        DataStore dataStore = DataStoreUtils.acquireDataStore(params,sc);//DataStoreFinder.getDataStore(params);
 
-        System.out.println("***" + layerName);
-     /*  Map params = new HashMap();
-params.put(PostgisNGDataStoreFactory.DBTYPE.key, "postgis");
-params.put(PostgisNGDataStoreFactory.HOST.key, "localhost");
-params.put(PostgisNGDataStoreFactory.PORT.key, 2345);
-params.put(PostgisNGDataStoreFactory.SCHEMA.key, "public");
-params.put(PostgisNGDataStoreFactory.DATABASE.key, "spatialdb");
-params.put(PostgisNGDataStoreFactory.USER.key, "postgres");
-params.put(PostgisNGDataStoreFactory.PASSWD.key, "postgres");*/
+        try {
 
-         //DefaultRegistry registry = new DefaultRegistry();
-         //registry.addDataStore("spatialdb", params);
+            if (dataStore == null)
+                    throw new Exception("Could not find datastore for this layer");
+            else {
+                FeatureSource layer = dataStore.getFeatureSource(layerName);
 
-        //File file = new File("/media/raid-storage/ALA Data/ALA - Dave Martin Data/data/ibra/ibra_reg_shape.shp");
-        //params.put("url",file.toURL());
-
-        try
-        {
-
-        DataStore dataStore = DataStoreFinder.getDataStore(params);
-
-        if (dataStore == null)
-                this.theFeature = "no data store";
-        else
-        {
-        FeatureSource layer = dataStore.getFeatureSource(layerName);
-        FeatureCollection features = layer.getFeatures(Query.ALL);
-        Feature feature = (Feature)(features.toArray())[0]; //features().next();
-        if (feature == null)
-            this.theFeature = "cries";
-        else
-            this.theFeature = feature.getIdentifier();
+                FeatureIterator features = layer.getFeatures(CQL.toFilter(gc.getIdAttributeName(layerName) + "= '" + featureName + "'")).features();
+//                System.out.println("*****" + features.size());
+//                Feature[] featuresArray = (Feature[])features.toArray();
+//                Feature feature = featuresArray[0];
+                if (features.hasNext()){
+                     Feature feature = (Feature) features.next();
+                    this.name = feature.getProperty(gc.getNameAttributeName(layerName)).getValue().toString();
+    //                Collection<Property> properties = feature.g;
+    //                for(Property property : properties) {
+    //                    this.properties.add(property); // = feature.getProperties().toArray()[];
+    //               }
+                }
+                else
+                    throw new Exception("Could not find feature");
+                    
+               
+            }
         }
+        finally {
+             dataStore.dispose();
         }
-        catch(Exception e)
-        {
-         e.printStackTrace();
-         this.theFeature = "fail";
-        }
-        
-
     }
 }
