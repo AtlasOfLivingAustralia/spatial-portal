969f5b660de6cc5574ace0dbc9f202fe8e6f6e1b
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-04-16 09:55:28 +0000

    Feature service now returns all the available properties of the feature.  Gazetteer Config includes values for all the gazetter layers - still hardcoded. Test cases updated. Fixed Gazetteer search so that it can handle search results with varying number of fields.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/gazetteer-search@107 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/rest/GazetteerConfig.java
+++ src/main/java/org/ala/rest/GazetteerConfig.java
@@ -6,16 +6,29 @@ public class GazetteerConfig {
 
     public GazetteerConfig(File file) {
         //open the gazetteer.xml
-
+            //FIXME: read xml file
         //read in the  values
     }
 
     public String getIdAttributeName(String layerName) {
         //returns the gazetteer identifier for the layer
+        //FIXME: read from xml
         if (layerName.contentEquals("GeoRegionFeatures"))
                return "id";
         if (layerName.contentEquals("NamedPlaces"))
                return "NAME";
+        if (layerName.contentEquals("aus0"))
+               return "name_0";
+        if (layerName.contentEquals("aus1"))
+               return "name_1";
+        if (layerName.contentEquals("aus2"))
+               return "name_2";
+        if (layerName.contentEquals("imcra4_pb"))
+               return "pb_name";
+        if (layerName.contentEquals("ibra_reg_shape"))
+               return "reg_name";
+        if (layerName.contentEquals("all_gaz_2008"))
+               return "record_id";
         else
             return "fixme";
     }
@@ -27,6 +40,18 @@ public class GazetteerConfig {
                return "name";
         if (layerName.contentEquals("NamedPlaces"))
                return "NAME";
+         if (layerName.contentEquals("aus0"))
+               return "name_0";
+        if (layerName.contentEquals("aus1"))
+               return "name_1";
+        if (layerName.contentEquals("aus2"))
+               return "name_2";
+        if (layerName.contentEquals("imcra4_pb"))
+               return "pb_name";
+        if (layerName.contentEquals("ibra_reg_shape"))
+               return "reg_name";
+        if (layerName.contentEquals("all_gaz_2008"))
+               return "name";
         else
             return "fixme";
     }
