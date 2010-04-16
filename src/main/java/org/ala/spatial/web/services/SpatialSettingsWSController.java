6adf1221d8f43b55581c6d487a17c9db5946e137
funkycoda <ajay.ranipeta@gmail.com>
2010-04-16 01:20:03 +0000

    - New version of SpeciesList
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/alaspatial@103 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/spatial/web/services/SpatialSettingsWSController.java
+++ src/main/java/org/ala/spatial/web/services/SpatialSettingsWSController.java
@@ -1,8 +1,13 @@
 package org.ala.spatial.web.services;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import org.ala.spatial.analysis.tabulation.SPLFilter;
 import org.ala.spatial.analysis.tabulation.SamplingService;
 import org.ala.spatial.analysis.tabulation.SpeciesListIndex;
 import org.ala.spatial.util.Layer;
@@ -22,11 +27,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SpatialSettingsWSController {
 
     private SpatialSettings ssets;
-
     private List<Layer> _layers;
 
     @RequestMapping(value = "/layers/environmental", method = RequestMethod.GET)
-    public @ResponseBody Layer[] envListAsLayers(HttpServletRequest req) {
+    public
+    @ResponseBody
+    Layer[] envListAsLayers(HttpServletRequest req) {
 
         ssets = new SpatialSettings();
 
@@ -43,7 +49,9 @@ public class SpatialSettingsWSController {
     }
 
     @RequestMapping(value = "/layers/environmental/string", method = RequestMethod.GET)
-    public @ResponseBody String environmentalLayersAsString(HttpServletRequest req) {
+    public
+    @ResponseBody
+    String environmentalLayersAsString(HttpServletRequest req) {
 
         ssets = new SpatialSettings();
 
@@ -64,7 +72,9 @@ public class SpatialSettingsWSController {
     }
 
     @RequestMapping(value = "/layers/contextual/string", method = RequestMethod.GET)
-    public @ResponseBody String contextualLayersAsString(HttpServletRequest req) {
+    public
+    @ResponseBody
+    String contextualLayersAsString(HttpServletRequest req) {
 
         ssets = new SpatialSettings();
 
@@ -85,27 +95,78 @@ public class SpatialSettingsWSController {
     }
 
     @RequestMapping(value = "/layer/{layer}/extents", method = RequestMethod.GET)
-    public @ResponseBody String getLayerExtents(@PathVariable  String layer) {
-
-        String layer_filename = SamplingService.layerDisplayNameToName(layer.replaceAll("_", " "));
-
-        String extents = SpeciesListIndex.getLayerExtents(layer_filename);
-
-        if (extents == null) extents = "Layer extents not available.";
-
+    public
+    @ResponseBody
+    String getLayerExtents(@PathVariable String layer) {
+        String extents = "";
+        try {
+            layer = URLDecoder.decode(layer, "UTF-8");
+            String layer_filename = SamplingService.layerDisplayNameToName(layer.replaceAll("_", " "));
+            extents = SpeciesListIndex.getLayerExtents(layer_filename);
+            if (extents == null) {
+                extents = "Layer extents not available.";
+            }
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(SpatialSettingsWSController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return extents;
     }
 
     @RequestMapping(value = "/layer/{layer}/metadata", method = RequestMethod.GET)
-    public @ResponseBody String getLayerMetadata(@PathVariable  String layer) {
-
-        String layer_filename = SamplingService.layerDisplayNameToName(layer.replaceAll("_", " "));
-
-        String metadata= SamplingService.getLayerMetaData(layer_filename);
-
-        if (metadata == null) metadata = "Layer metadata not available.";
-
+    public
+    @ResponseBody
+    String getLayerMetadata(@PathVariable String layer) {
+        String metadata = "";
+        try {
+            layer = URLDecoder.decode(layer, "UTF-8");
+            String layer_filename = SamplingService.layerDisplayNameToName(layer.replaceAll("_", " "));
+            metadata = SamplingService.getLayerMetaData(layer_filename);
+            if (metadata == null) {
+                metadata = "Layer metadata not available.";
+            }
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(SpatialSettingsWSController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return metadata;
+
     }
 
+    @RequestMapping(value = "/layer/{layer}/splfilter", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    SPLFilter getSPLFilter(@PathVariable String layer) {
+
+        try {
+
+            layer = URLDecoder.decode(layer, "UTF-8");
+
+            Layer l = null;
+
+            ssets = new SpatialSettings();
+
+            Layer[] _layerlist = ssets.getEnvironmentalLayers();
+
+            for (int i = 0; i < _layerlist.length; i++) {
+                if (_layerlist[i].display_name.equalsIgnoreCase(layer)) {
+                    l = _layerlist[i];
+                }
+            }
+
+            if (l == null) {
+                _layerlist = ssets.getContextualLayers();
+
+                for (int i = 0; i < _layerlist.length; i++) {
+                    if (_layerlist[i].display_name.equalsIgnoreCase(layer)) {
+                        l = _layerlist[i];
+                    }
+                }
+            }
+            return SpeciesListIndex.getLayerFilter(l);
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+        return null;
+    }
 }
