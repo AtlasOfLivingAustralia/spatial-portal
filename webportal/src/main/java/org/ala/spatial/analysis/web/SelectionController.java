3cb1c51ae0c006e98ec80bb7049b8913dbf9f650
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-05-18 17:13:29 +0000

    Shows sorted list of species on bounding box complete, download button works also.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@134 73def43e-0e3b-11df-a5af-b38b39d06e06

--- webportal/src/main/java/org/ala/spatial/analysis/web/SelectionController.java
+++ webportal/src/main/java/org/ala/spatial/analysis/web/SelectionController.java
@@ -5,30 +5,51 @@ import au.org.emii.portal.composer.UtilityComposer;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+import java.lang.String;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.List;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import org.xml.sax.SAXException;
+
 import org.zkoss.zhtml.Messagebox;
-import org.zkoss.zk.au.AuRequest;
-import org.zkoss.zk.au.Command;
-import org.zkoss.zk.au.ComponentCommand;
+
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.Events;
+
 import org.zkoss.zk.ui.event.InputEvent;
-import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Label;
+
 import org.zkoss.zul.ListModelArray;
 import org.zkoss.zul.Listbox;
+
+
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Popup;
+import org.zkoss.zul.SimpleListModel;
 import org.zkoss.zul.Textbox;
-import org.zkoss.zul.Window;
+
 
 /**
  *
@@ -45,6 +66,7 @@ public class SelectionController extends UtilityComposer {
     public Label results_label;
     String[] results = null;
     int results_pos;
+    SortedSet speciesSet;
 
     @Override
     public void afterCompose() {
@@ -81,33 +103,38 @@ public class SelectionController extends UtilityComposer {
         try {
             //Messagebox.show(selectionGeom.getValue());
             wfsQuerySelection(selectionGeom.getValue());
-            popup_results.open(30, 30);
+            
         } catch (Exception e) {//FIXME
         }
 
     }
 
     public void wfsQuerySelection(String selectionGeom) {
-        String baseQueryXML = "<wfs:GetFeature service=\"WFS\" version=\"1.1.0\" xmlns:topp=\"http://www.openplans.org/topp\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\"> <wfs:Query typeName=\"ALA:occurrencesv1\"> <wfs:PropertyName>ALA:phylum</wfs:PropertyName><ogc:Filter><ogc:BBOX><ogc:PropertyName>the_geom</ogc:PropertyName><gml:Envelope srsName=\"http://www.opengis.net/gml/srs/epsg.xml#4326\"><gml:lowerCorner>LOWERCORNER</gml:lowerCorner><gml:upperCorner>UPPERCORNER</gml:upperCorner></gml:Envelope></ogc:BBOX></ogc:Filter></wfs:Query></wfs:GetFeature>";
-        String upperCorner = selectionGeom.replace("POLYGON(","").replace(")","").split(",")[3].toString();//"146.6064453125 -38.162109375";
-        String lowerCorner = selectionGeom.replace("POLYGON(","").replace("(","").split(",")[1].toString(); //"146.1669921875 -38.4697265625";
+        String baseQueryXML = "<wfs:GetFeature service=\"WFS\" version=\"1.1.0\" xmlns:topp=\"http://www.openplans.org/topp\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\"> <wfs:Query typeName=\"ALA:occurrencesv1\"> <wfs:PropertyName>ALA:species</wfs:PropertyName><ogc:Filter><ogc:BBOX><ogc:PropertyName>the_geom</ogc:PropertyName><gml:Envelope srsName=\"http://www.opengis.net/gml/srs/epsg.xml#4326\"><gml:lowerCorner>LOWERCORNER</gml:lowerCorner><gml:upperCorner>UPPERCORNER</gml:upperCorner></gml:Envelope></ogc:BBOX></ogc:Filter></wfs:Query></wfs:GetFeature>";
+        String upperCorner = selectionGeom.replace("POLYGON(","").replace(")","").split(",")[3].toString();
+        String lowerCorner = selectionGeom.replace("POLYGON(","").replace("(","").split(",")[1].toString(); 
         baseQueryXML = baseQueryXML.replace("UPPERCORNER", upperCorner);
         baseQueryXML = baseQueryXML.replace("LOWERCORNER", lowerCorner);
         
         //       String completeQuery = baseQuery + selection + ")";
         try {
-           // Messagebox.show(lowerCorner + " " + upperCorner);
-            //Messagebox.show(baseQueryXML);
+           
             String response = POSTRequest(baseQueryXML);
-            //Messagebox.show(response);
+           // Messagebox.show(response);
+         
         } catch (Exception e) { //FIXME
         }
     }
 
+    /**
+     * Sends and POST to the wfs
+     * @param query
+     * @return
+     */
     public String POSTRequest(String query) {
         try {
             // Construct data
-            String data = URLEncoder.encode(query, "UTF-8");// + "=" + URLEncoder.encode(query, "UTF-8");
+            //String data = URLEncoder.encode(query, "UTF-8");// + "=" + URLEncoder.encode(query, "UTF-8");
             
 
             // Send data 
@@ -128,12 +155,60 @@ public class SelectionController extends UtilityComposer {
             while ((line = rd.readLine()) != null) {
                 response+=line;
             }
+            //Parse the response
+            String speciesList = parse(response);
             wr.close();
-            rd.close();
-            return response;
+           // rd.close();
+            return speciesList;
         } catch (Exception e) {
-            return "fail";
+            return e.getMessage();
         }
+
+    }
+
+    /**
+     * Takes a WFS response and returns species list
+     * @param xmlResponse The response of an WFS query.
+     * @return
+     */
+    public String parse(String responseXML) throws ParserConfigurationException, XPathExpressionException,SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setNamespaceAware(true);
+        
+         DocumentBuilder builder = domFactory.newDocumentBuilder();
+         
+        InputStream is = new java.io.ByteArrayInputStream(responseXML.getBytes());
+        
+        Document resultDoc = builder.parse(is);
+
+//        try {
+//        Messagebox.show(responseXML);
+//        } catch (Exception e) {}
+        //Get a list of names
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        //Ugly xpath expression - using local-name() so that I can ignore namespaces
+        XPathExpression speciesExpr = xpath.compile("//*[local-name()='FeatureCollection']//*[local-name()='featureMembers']//*[local-name()='occurrencesv1']//*[local-name()='species']/text()");
+
+        NodeList species = (NodeList) speciesExpr.evaluate(resultDoc, XPathConstants.NODESET);
+        speciesSet = new TreeSet();
+        for (int i = 0; i < species.getLength(); i++) {
+                 speciesSet.add((String) species.item(i).getNodeValue());
+        }
+
+        results = (String[])speciesSet.toArray(new String[speciesSet.size()]);
+        popup_listbox_results.setModel(new SimpleListModel(speciesSet.toArray()));
+        popup_results.open(30, 30);
+//        for(Object speciesName : speciesSet) {
+//            Listitem li = new Listitem();
+//            Listcell lc = new Listcell();
+//            lc.setLabel((String)speciesName);
+//            lc.setParent(li);
+//            popup_listbox_results.addItemToSelection(li);
+//        }
+        return String.valueOf(speciesSet.size());
+        
+        
 
     }
 
@@ -177,74 +252,75 @@ public class SelectionController extends UtilityComposer {
     public void onClick$download() {
 //		SPLFilter [] layer_filters = getSelectedFilters();
 //		if(layer_filters != null){
-//			StringBuffer sb = new StringBuffer();
-//			for(String s : results){
-//				sb.append(s);
-//				sb.append("\r\n");
-//			}
-//			org.zkoss.zhtml.Filedownload.save(sb.toString(),"text/plain","filter.csv");
+			StringBuffer sb = new StringBuffer();
+			for(String s : results){
+				sb.append(s);
+				sb.append("\r\n");
+			}
+			org.zkoss.zhtml.Filedownload.save(sb.toString(),"text/plain","species.csv");
 //		}else{
+
+    }
+    
+
+//    public void onClick$results_prev(Event event) {
+//        if (results_pos == 0) {
+//            return;
+//        }
 //
-//		}
-    }
-
-    public void onClick$results_prev(Event event) {
-        if (results_pos == 0) {
-            return;
-        }
-
-        seekToResultsPosition(results_pos - 15);
-    }
-
-    public void onClick$results_next(Event event) {
-        if (results_pos + 15 >= results.length) {
-            return;
-        }
-
-        seekToResultsPosition(results_pos + 15);
-    }
-
-    public void onChanging$popup_results_seek(InputEvent event) {
-        //seek results list
-        String search_for = event.getValue();
-        //if(search_for.length() > 1){
-        //	search_for = search_for.substring(0,1).toUpperCase() + search_for.substring(1,search_for.length()).toLowerCase();
-		/*}else*/ if (search_for.length() > 0) {
-            search_for = search_for.toLowerCase();
-        }
-
-        int pos = java.util.Arrays.binarySearch(results, search_for);
-        System.out.println("seek to: " + pos + " " + search_for);
-        if (pos < 0) {
-            pos = (pos * -1) - 1;
-        }
-        seekToResultsPosition(pos);
-    }
-
-    void seekToResultsPosition(int newpos) {
-        results_pos = newpos;
-
-        if (results_pos < 0) {
-            results_pos = 0;
-        }
-        if (results_pos >= results.length) {
-            results_pos = results.length - 1;
-        }
-
-        int sz = results_pos + 15;
-        if (results.length < sz) {
-            sz = results.length;
-        }
-
-        String[] list = new String[sz - results_pos];
-        int i;
-        for (i = results_pos; i < sz; i++) {
-            list[i - results_pos] = results[i];
-        }
-
-        ListModelArray slm = new ListModelArray(list, true);
-
-        popup_listbox_results.setModel(slm);
-        results_label.setValue(results_pos + " to " + (sz) + " of " + results.length);
-    }
+//        seekToResultsPosition(results_pos - 15);
+//    }
+//
+//    public void onClick$results_next(Event event) {
+//        if (results_pos + 15 >= results.length) {
+//            return;
+//        }
+//
+//        seekToResultsPosition(results_pos + 15);
+//    }
+//
+//    public void onChanging$popup_results_seek(InputEvent event) {
+//        //seek results list
+//        String search_for = event.getValue();
+//        //if(search_for.length() > 1){
+//        //	search_for = search_for.substring(0,1).toUpperCase() + search_for.substring(1,search_for.length()).toLowerCase();
+//		/*}else*/
+//        if (search_for.length() > 0) {
+//            search_for = search_for.toLowerCase();
+//        }
+//
+//        int pos = java.util.Arrays.binarySearch(results, search_for);
+//        System.out.println("seek to: " + pos + " " + search_for);
+//        if (pos < 0) {
+//            pos = (pos * -1) - 1;
+//        }
+//        seekToResultsPosition(pos);
+//    }
+//
+//    void seekToResultsPosition(int newpos) {
+//        results_pos = newpos;
+//
+//        if (results_pos < 0) {
+//            results_pos = 0;
+//        }
+//        if (results_pos >= results.length) {
+//            results_pos = results.length - 1;
+//        }
+//
+//        int sz = results_pos + 15;
+//        if (results.length < sz) {
+//            sz = results.length;
+//        }
+//
+//        String[] list = new String[sz - results_pos];
+//        int i;
+//        for (i = results_pos; i < sz; i++) {
+//            list[i - results_pos] = results[i];
+//        }
+//
+//        ListModelArray slm = new ListModelArray(list, true);
+//
+//        popup_listbox_results.setModel(slm);
+//        results_label.setValue(results_pos + " to " + (sz) + " of " + results.length);
+//    }
 }
