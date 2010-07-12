917d21c46c66dd623fc15191b733d0c8f8dc3773
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2010-07-12 03:51:17 +0000

    Description field now only includes description attributes.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@291 73def43e-0e3b-11df-a5af-b38b39d06e06

--- gazetteer-search/src/main/java/org/ala/rest/SearchResultItem.java
+++ gazetteer-search/src/main/java/org/ala/rest/SearchResultItem.java
@@ -27,22 +27,23 @@ public class SearchResultItem {
             if (field.name().contentEquals("name")) {
                 this.name = field.stringValue();
             }
-            if (field.name().contentEquals("serial")) {
+            else if (field.name().contentEquals("serial")) {
                 this.serial = field.stringValue();
             }
-            if (field.name().contentEquals("state")) {
+            else if (field.name().contentEquals("state")) {
                 this.state = field.stringValue();
             }
-            if (field.name().contentEquals("type")) {
+            else if (field.name().contentEquals("type")) {
                 this.type = field.stringValue();
             }
-            if (field.name().contentEquals("id")) {
+            else if (field.name().contentEquals("id")) {
                 id = field.stringValue();
             }
             else
                 this.description+=field.stringValue() + ",";
         }
-        description  = description.substring(0,description.length()-1);
+        if (!description.contentEquals(""))
+            description  = description.substring(0,description.length()-1);
         if (includeLink == true) {
             this.link = "/geoserver/rest/gazetteer/";
             this.link += this.type + '/' + id.replace(" ", "_") + ".json";
