fde95495d13d77c40794d67606571c0eae02c2cf
funkycoda <ajay.ranipeta@gmail.com>
2010-09-14 00:06:06 +0000

    - added a small fix so that the gaz returns something when first loaded (features starting with the letter 'a').
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@377 73def43e-0e3b-11df-a5af-b38b39d06e06

--- webportal/src/main/java/org/ala/spatial/gazetteer/AutoComplete.java
+++ webportal/src/main/java/org/ala/spatial/gazetteer/AutoComplete.java
@@ -122,6 +122,12 @@ public class AutoComplete extends Combobox {
         BasicHttpContext localcontext = new BasicHttpContext();
         String searchString = val.trim().replaceAll("\\s+", "+");
 
+        // check if searchString is empty, if so, set it to "a"
+        // this is a hack to make sure a valid xml is returned
+        // rather than just a "1"
+        // TODO: angus fix this?
+        searchString = (searchString.equals(""))?"a":searchString; 
+
         DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
         domFactory.setNamespaceAware(true); 
 
