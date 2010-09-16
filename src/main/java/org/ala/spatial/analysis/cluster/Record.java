6a3adf2d3cd46557419f1c261ccfe3da8b5d29ef
funkycoda <ajay.ranipeta@gmail.com>
2010-09-16 23:16:18 +0000

    - Clustering (Issue 152)
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/alaspatial@385 73def43e-0e3b-11df-a5af-b38b39d06e06

---
+++ src/main/java/org/ala/spatial/analysis/cluster/Record.java
@@ -0,0 +1,58 @@
+package org.ala.spatial.analysis.cluster;
+
+/**
+ *
+ * @author ajay
+ */
+public class Record {
+
+    private double latitude;
+    private double longitude;
+    private String id;
+    private String name;
+
+    public Record(String id, String name, double longitude, double latitude) {
+        this.id = id;
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + "_" + name + " at " + longitude + ", " + latitude;
+    }
+
+}
