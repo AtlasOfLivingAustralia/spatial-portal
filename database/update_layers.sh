42145c38100c0109ae0c3bc4887c3ee8a31cf749
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-02-11 00:45:35 +0000

    Updated layers table to reflect new category updates
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@750 73def43e-0e3b-11df-a5af-b38b39d06e06

--- database/update_layers.sh
+++ database/update_layers.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+#
+# This script restores the layers table and associated table sequence
+# Gavin Jackson 11/2/2011
+
 PSQL_CMD=/usr/local/pgsql/bin/psql
+
+#allows script to work on local (OSX) postgres environment
+if [ -e /Library/PostgreSQL/9.0/bin/psql ]
+then
+  PSQL_CMD=/Library/PostgreSQL/9.0/bin/psql
+fi
 
 # run layers create/update script
 $PSQL_CMD -U postgres spatialdb -f ./layers.sql
