ef1c4a9fe6607157f169bb753ae89967b6bf356f
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2011-02-17 23:41:12 +0000

    Fix for issue #351.  Post process lucene results and give extra weight to names with search term as prefix.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/branches/gazetteer-tidy@758 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/rest/Search.java
+++ src/main/java/org/ala/rest/Search.java
@@ -20,6 +20,8 @@ import java.util.Map;
 import java.util.HashMap;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import java.util.Arrays;
+
 import java.util.logging.Logger;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.geoserver.platform.GeoServerExtensions;
@@ -32,92 +34,110 @@ import org.geotools.util.logging.Logging;
 @XStreamAlias("search")
 public class Search {
 
-    private static final Logger logger = Logging.getLogger("org.ala.rest.Search");
-    @XStreamAlias("results")
-    ArrayList<SearchResultItem> results;
-    @XStreamAlias("xmlns:xlink")
-    @XStreamAsAttribute
-    String xlink = "http://www.w3.org/1999/xlink";
+	private static final Logger logger = Logging.getLogger("org.ala.rest.Search");
+	@XStreamAlias("results")
+	ArrayList<SearchResultItem> results;
+	@XStreamAlias("xmlns:xlink")
+	@XStreamAsAttribute
+	String xlink = "http://www.w3.org/1999/xlink";
 
-    /***
-     *
-     * @return a HashMap representation of the resource - which will be serialized into xml/json
-     */
-    public Map getMap() {
-        HashMap<String, ArrayList<SearchResultItem>> resultsMap = new HashMap<String, ArrayList<SearchResultItem>>();
-        resultsMap.put("results", this.results);
-        return resultsMap;
-    }
+	/***
+	 *
+	 * @return a HashMap representation of the resource - which will be serialized into xml/json
+	 */
+	public Map getMap() {
+		HashMap<String, ArrayList<SearchResultItem>> resultsMap = new HashMap<String, ArrayList<SearchResultItem>>();
+		resultsMap.put("results", this.results);
+		return resultsMap;
+	}
 
-    public ArrayList<SearchResultItem> getResults() {
-        return this.results;
-    }
-    /**
-     * If no type is specified, assume that it is a name based search
-     * @param searchTerms
-     * @param layers
-     */
-    public Search(String searchTerms, String[] layers){
-        this(searchTerms, layers,"name");
-    }
-    /**
-     * Searches for a feature within a layer based on name.
-     * @param searchTerms
-     * @param layers
-     * @param type
-     */
-    public Search(String searchTerms, String[] layers, String type) {
-        results = new ArrayList<SearchResultItem>();
-        GazetteerConfig gc = GeoServerExtensions.bean(GazetteerConfig.class);
-        String layerSearch = "";
+	public ArrayList<SearchResultItem> getResults() {
+		return this.results;
+	}
 
-        if (layers.length > 0) {
-            layerSearch = "layerName:(";
-            int count = 0;
-            for (String layerName : layers) {
-                layerSearch += layerName;
-                count++;
-                if (count < layers.length) {
-                    layerSearch += " OR ";
-                }
-            }
-            layerSearch += ")";
-        }
-        logger.finer("layerSearch is " + layerSearch);
+	/**
+	 * If no type is specified, assume that it is a name based search
+	 * @param searchTerms
+	 * @param layers
+	 */
+	public Search(String searchTerms, String[] layers) {
+		this(searchTerms, layers, "name");
+	}
 
-        try {
-            //Get the geoserver data directory from the geoserver instance
-            File file = new File(GeoserverDataDirectory.getGeoserverDataDirectory(), "gazetteer-index");
-            IndexSearcher is = new IndexSearcher(FSDirectory.open(file));
-            String[] searchFields = {"name","layerName"};
+	/**
+	 * Searches for a feature within a layer based on name.
+	 * @param searchTerms
+	 * @param layers
+	 * @param type
+	 */
+	public Search(String searchTerms, String[] layers, String type) {
+		results = new ArrayList<SearchResultItem>();
+		GazetteerConfig gc = GeoServerExtensions.bean(GazetteerConfig.class);
+		String layerSearch = "";
 
-            if (type.compareTo("id") == 0){
-                searchFields = new String[] {"id", "layerName"};
-                logger.finer("searching based on id");
-            }
-            
-            searchTerms.replace("_", " ");
+		if (layers.length > 0) {
+			layerSearch = "layerName:(";
+			int count = 0;
+			for (String layerName : layers) {
+				layerSearch += layerName;
+				count++;
+				if (count < layers.length) {
+					layerSearch += " OR ";
+				}
+			}
+			layerSearch += ")";
+		}
+		logger.finer("layerSearch is " + layerSearch);
 
-            MultiFieldQueryParser qp = new MultiFieldQueryParser(Version.LUCENE_CURRENT, searchFields, new StandardAnalyzer(Version.LUCENE_CURRENT));
-            qp.setDefaultOperator(qp.AND_OPERATOR);
-            Query nameQuery = qp.parse(searchTerms.toLowerCase() + " AND " + layerSearch);
+		try {
+			//Get the geoserver data directory from the geoserver instance
+			File file = new File(GeoserverDataDirectory.getGeoserverDataDirectory(), "gazetteer-index");
+			IndexSearcher is = new IndexSearcher(FSDirectory.open(file));
+			String[] searchFields = {"name", "layerName"};
 
-            //TODO: instead of 20 - should be variable and paging?
-            TopDocs topDocs = is.search(nameQuery, 20);
+			if (type.compareTo("id") == 0) {
+				searchFields = new String[]{"id", "layerName"};
+				logger.finer("searching based on id");
+			}
 
-            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document doc = is.doc(scoreDoc.doc);
-                List<Fieldable> fields = doc.getFields();
-                results.add(new SearchResultItem(fields, true, new Float(scoreDoc.score)));
-            }
-            is.close();
-        } catch (IOException e1) {
-            //FIXME: Log error - return http error code?
-            logger.severe("An error occurred in search.");
-            logger.severe(ExceptionUtils.getFullStackTrace(e1));
-        } catch (ParseException e3) {
-            logger.severe("An error occurred parsing search terms.");
-            logger.severe(ExceptionUtils.getFullStackTrace(e3));
-        }
-    }
+			searchTerms.replace("_", " ");
+
+			MultiFieldQueryParser qp = new MultiFieldQueryParser(Version.LUCENE_CURRENT, searchFields, new StandardAnalyzer(Version.LUCENE_CURRENT));
+			qp.setDefaultOperator(qp.AND_OPERATOR);
+			Query nameQuery = qp.parse(searchTerms.toLowerCase() + " AND " + layerSearch);
+
+
+			//TODO: instead of 20 - should be variable and paging?
+			TopDocs topDocs = is.search(nameQuery, 20);
+
+
+
+
+			HashMap scoreDocs = new HashMap();
+			for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+				Document doc = is.doc(scoreDoc.doc);
+				if (doc.get("name").toLowerCase().startsWith(searchTerms.toLowerCase().replace("*","")))
+					scoreDoc.score += 3;
+				scoreDocs.put(scoreDoc.score, scoreDoc);
+			}
+
+			Object[] keys = scoreDocs.keySet().toArray();
+			Arrays.sort(keys);
+			for (int i =keys.length -1; i >= 0;i--) {
+				ScoreDoc scoreDoc = (ScoreDoc)scoreDocs.get(keys[i]);
+				Document doc = is.doc(scoreDoc.doc);
+				List<Fieldable> fields = doc.getFields();
+				float score = scoreDoc.score;
+				results.add(new SearchResultItem(fields, true, score));
+			}
+			is.close();
+		} catch (IOException e1) {
+			//FIXME: Log error - return http error code?
+			logger.severe("An error occurred in search.");
+			logger.severe(ExceptionUtils.getFullStackTrace(e1));
+		} catch (ParseException e3) {
+			logger.severe("An error occurred parsing search terms.");
+			logger.severe(ExceptionUtils.getFullStackTrace(e3));
+		}
+	}
 }
