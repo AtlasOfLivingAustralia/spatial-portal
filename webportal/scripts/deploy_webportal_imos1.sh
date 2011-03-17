5630581aff456b04245556c88d3cefa4b570dcf8
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2011-03-17 01:46:21 +0000

    Deleting trunk webportal to make way for branch webportal to become trunk. This trunk is tagged.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@807 73def43e-0e3b-11df-a5af-b38b39d06e06

--- webportal/scripts/deploy_webportal_imos1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-SCRIPT_DIR=`dirname $0`
-BACKUP_DIR="backup"
-WARFILE_BASENAME="webportal"
-TOMCAT_PATH="/usr/local/tomcat/instance_00_webportal"
-SERVER="imos1.ersa.edu.au"
-
-. $SCRIPT_DIR/deploy_warfile.sh
--- webportal/src/main/java/au/org/emii/portal/wms/ServiceProcessor.java
+++
@@ -1,7 +0,0 @@
-package au.org.emii.portal.wms;
-import au.org.emii.portal.menu.MapLayer;
-import au.org.emii.portal.config.xmlbeans.Service;
-
-public interface ServiceProcessor {
-	public MapLayer service(Service service);
-}
