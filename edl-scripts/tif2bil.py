9951de54f1e624d0a6cc6e5c76290088d17a3cf7
angus.i.macaulay@gmail.com <angus.i.macaulay@gmail.com>
2011-05-11 03:18:53 +0000

    removed int16 default format from tif2bil script
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@972 73def43e-0e3b-11df-a5af-b38b39d06e06

--- edl-scripts/tif2bil.py
+++ edl-scripts/tif2bil.py
@@ -9,7 +9,7 @@ for root, dirs, files in os.walk(edlconfig.dataset):
 			layername = name.replace(".tif","")
 			print(layername)		
 		#	os.system(edlconfig.gdalapps+"/gdal_translate -of EHdr " + os.path.join(root,name) + " "  + os.path.join(root,layername) + ".bil")
-			command = edlconfig.gdalapps+"/gdalwarp -ot int16 -te 109.51 -44.37 157.28 -8.19 -tr 0.01 -0.01 -s_srs '" + edlconfig.s_srs + "' -t_srs '" + edlconfig.t_srs + "' -of EHdr -srcnodata -9999 -dstnodata -9999 " +  os.path.join(root,layername) + ".tif " + os.path.join(root,layername) + ".bil"
+			command = edlconfig.gdalapps+"/gdalwarp -te 109.51 -44.37 157.28 -8.19 -tr 0.01 -0.01 -s_srs '" + edlconfig.s_srs + "' -t_srs '" + edlconfig.t_srs + "' -of EHdr -srcnodata -9999 -dstnodata -9999 " +  os.path.join(root,layername) + ".tif " + os.path.join(root,layername) + ".bil"
 
 			print(command)
 			os.system(command)
