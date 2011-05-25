b2fd41b01bc9854af3d808613af3c3f6ec2f67ec
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-05-25 03:25:26 +0000

    added ne country layer to layer table (dev)
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@1020 73def43e-0e3b-11df-a5af-b38b39d06e06

--- database/update_layers_dev.sh
+++ database/update_layers_dev.sh
@@ -17,4 +17,5 @@ $PSQL_CMD -U postgres spatialdb -f ./layers_dev.sql
 # add updates here ...
 $PSQL_CMD -U postgres spatialdb -f ./layers_dev_updates/001_ger_updates_12052011.sql
 $PSQL_CMD -U postgres spatialdb -f ./layers_dev_updates/002_ger_updates_19052011.sql
+$PSQL_CMD -U postgres spatialdb -f ./layers_dev_updates/003_ne_world_25052011.sql
 
