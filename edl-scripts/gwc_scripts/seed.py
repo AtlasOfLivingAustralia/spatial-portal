9d71e73095471ecfa5c09bd4ed02b63107179123
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-06-21 07:12:48 +0000

    added a gwc seed script
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@1137 73def43e-0e3b-11df-a5af-b38b39d06e06

---
+++ edl-scripts/gwc_scripts/seed.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import os
+import seed_config
+
+for layer in seed_config.layers:
+        curlstring="curl -u " + seed_config.geoserver_userpass + " -XPOST -H 'Content-type: text/xml' -d '<?xml version=\"1.0\" encoding=\"UTF-8\"?><seedRequest><name>" + layer + "</name><srs><number>900913</number></srs><zoomStart>0</zoomStart><zoomStop>12</zoomStop><format>image/png</format><type>seed</type><threadCount>2</threadCount></seedRequest>' " + seed_config.geoserver_url + "/geoserver/gwc/rest/seed/" + layer + ".xml"
+        #print(curlstring)
+        os.system(curlstring)
+        print "seeded " + layer
