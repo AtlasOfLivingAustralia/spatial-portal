9d6f1eccde8bdbecc44ff7a7cfe9d90c0e6ee501
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-07-04 07:26:13 +0000

    updates to gwc seed scripts
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@1169 73def43e-0e3b-11df-a5af-b38b39d06e06

--- edl-scripts/gwc_scripts/purge.py
+++ edl-scripts/gwc_scripts/purge.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
+
+#This script removes tiles from the GWC cache
+#Ensure that seed_config.py has been configured before use
+
 import os
 import seed_config
 
 for layer in seed_config.layers:
-        curlstring="curl -u " + seed_config.geoserver_userpass + " -XPOST -H 'Content-type: text/xml' -d '<?xml version=\"1.0\" encoding=\"UTF-8\"?><seedRequest><name>" + layer + "</name><srs><number>900913</number></srs><zoomStart>0</zoomStart><zoomStop>12</zoomStop><format>image/png</format><type>truncate</type><threadCount>2</threadCount></seedRequest>' " + seed_config.geoserver_url + "/geoserver/gwc/rest/seed/" + layer + ".xml"
-        #print(curlstring)
+        curlstring="curl -u " + seed_config.geoserver_userpass + " -XPOST -H 'Content-type: text/xml' -d '<?xml version=\"1.0\" encoding=\"UTF-8\"?><seedRequest><name>" + layer + "</name><srs><number>900913</number></srs><zoomStart>" + seed_config.zoom_start + "</zoomStart><zoomStop>" + seed_config.zoom_stop + "</zoomStop><format>image/png</format><type>truncate</type><threadCount>2</threadCount></seedRequest>' " + seed_config.geoserver_url + "/geoserver/gwc/rest/seed/" + layer + ".xml"
         os.system(curlstring)
         print "truncated " + layer
---
+++ edl-scripts/gwc_scripts/seed_config_skel.py
@@ -0,0 +1,18 @@
+#This config file contains options for the gwc preseeding scripts
+geoserver_userpass = "admin:XXXXX"
+geoserver_url = "http://spatial-dev.ala.org.au:8082"
+
+#Zoom levels to create tiles for
+zoom_start = "0"
+zoom_stop = "10"
+
+#Geoserver layers to perform preseeding operation on
+
+#        All Expert Distributions
+layers = ["ALA:Rees_CAAB37009001Polygon","ALA:Rees_CAAB37007002Polygon","ALA:Rees_CAAB37007001Polygon","ALA:Rees_CAAB37006001Polygon","ALA:Rees_CAAB37010004Polygon","ALA:Rees_CAAB37005005Polygon","ALA:Rees_CAAB37008003Polygon","ALA:Rees_CAAB37005002Polygon","ALA:Rees_CAAB37011001Polygon","ALA:Rees_CAAB37008001Polygon","ALA:Rees_CAAB37005004Polygon","ALA:Rees_CAAB37005001Polygon","ALA:Rees_CAAB37012002Polygon","ALA:Rees_CAAB37007003Polygon","ALA:Rees_CAAB37010002Polygon","ALA:Rees_CAAB37010003Polygon"]
+
+#         Single Expert Distribution
+#layers = ["ALA:Rees_CAAB37007002Polygon"]
+
+#          Contextual Layers
+#layers = ["ALA:aus1","ALA:diwa_type_criteria","ALA:imcra4_pb","ALA:ibra_merged","ALA:aus2","ALA:sodicity","ALA:ibra_sub_merged","ALA:ramsar","ALA:nrm_regions_2010","ALA:australian_coral_ecoregions","ALA:ger_border_ranges","ALA:ger_geri_boundary_v102_australia","ALA:ger_analysis_boundary_v01","ALA:ger_hunter","ALA:ger_hunter_analysis_mask","ALA:ger_hunter_areas_of_interest","ALA:ger_upper_hunter_focus_area_v2","ALA:ger_kosciuszko_to_coast","ALA:ger_k2c_management_regions_oct2009","ALA:ger_slopes_to_summit","ALA:ger_s2s_priority_area_billabong_creek_v01","ALA:ger_s2s_priority_areas_v05","ALA:ne_world","ALA:east_afa_final"]
