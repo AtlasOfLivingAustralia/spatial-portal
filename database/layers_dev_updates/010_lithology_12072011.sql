f997303e4570acb4647c0e4ddda258d2bab93253
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-07-13 02:06:39 +0000

    updated lithology metadata
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@1187 73def43e-0e3b-11df-a5af-b38b39d06e06

--- database/layers_dev_updates/010_lithology_12072011.sql
+++ database/layers_dev_updates/010_lithology_12072011.sql
@@ -49,12 +49,12 @@ NULL,
 72.577, 
 -9.178, 
 167.998, 
-'The Surface Geology of Australia (2010 edition) is a seamless national coverage of outcrop and surficial geology, compiled for us
+E'The Surface Geology of Australia (2010 edition) is a seamless national coverage of outcrop and surficial geology, compiled for us
 e at or around 1:1 000 000 scale.  The data maps outcropping bedrock geology and unconsolidated or poorly consolidated regolith m
 aterial covering bedrock.  Geological units are represented as polygon and line geometries, and are attributed with information r
 egarding stratigraphic nomenclature and parentage, age, lithology, and primary data source.  The dataset also contains geological
  contacts, structural features such as faults and shears, and miscellaneous supporting lines like the boundaries of water and ice
- bodies.
+ bodies.<br/><br/>
 
 The dataset has been compiled from merging the seven State and Territory 1:1 000 000 scale surface geology datasets released by G
 eoscience Australia between 2006 and 2008, correcting errors and omissions identified in those datasets, addition of some offshor
@@ -62,85 +62,85 @@ e island territories, and updating stratigraphic attribute information to the be
 aphic Units Database (http://www.ga.gov.au/oracle/stratnames/index.jsp).  The map data were compiled largely from simplifying and
  edgematching existing 1:250 000 scale geological maps.  Where these maps were not current, more recent source maps, ranging in s
 cale from 1:50 000 to 1:1 000 000 were used.  In some areas where the only available geological maps were quite old and poorly lo
-cated, some repositioning of mapping using recent satellite imagery or geophysics was employed.
+cated, some repositioning of mapping using recent satellite imagery or geophysics was employed.<br/><br/>
 
-This data is freely available from Geoscience Australia under the Creative Commons Attribution 2.5 Australia Licence.
+This data is freely available from Geoscience Australia under the Creative Commons Attribution 2.5 Australia Licence.<br/><br/>
 
 
-It is recommended that these data be referred to as: 
+It is recommended that these data be referred to as: <br/><br/>
 
 Raymond, O.L., Retter, A.J., (editors), 2010.  Surface geology of Australia 1:1,000,000 scale, 2010 edition [Digital Dataset]
-Geoscience Australia, Commonwealth of Australia, Canberra.  http://www.ga.gov.au
+Geoscience Australia, Commonwealth of Australia, Canberra.  http://www.ga.gov.au<br/><br/>
 
-Specialised Geographic Information System (GIS) software is required to view this data.
+Specialised Geographic Information System (GIS) software is required to view this data.<br/><br/>
 
-Descriptions of MAP_SYMB attribute field:
-MAP_SYMB format  = Drxy
+Descriptions of MAP_SYMB attribute field:<br/>
+MAP_SYMB format  = Drxy<br/><br/>
 
-1. D = unit age.  Two letters may be used for units spanning for than one age periods.  
+1. D = unit age.  Two letters may be used for units spanning for than one age periods.<br/><br/>
 
-Cenozoic                Cz
-Quaternary              Q
-Mesozoic                Mz
-Cretaceous              K
-Jurassic                J
-Triassic                -R
-Paleozoic               Pz
-Permian                 P
-Carboniferous           C
-Devonian                D
-Silurian                S
-Ordovician              O
-Cambrian                -C
-Proterozoic             -P
-Neoproterozoic          N
-Mesoproterozoic         M
-Paleoproterozoic        L
-Archean                 A
+Cenozoic                Cz<br/>
+Quaternary              Q<br/>
+Mesozoic                Mz<br/>
+Cretaceous              K<br/>
+Jurassic                J<br/>
+Triassic                -R<br/>
+Paleozoic               Pz<br/>
+Permian                 P<br/>
+Carboniferous           C<br/>
+Devonian                D<br/>
+Silurian                S<br/>
+Ordovician              O<br/>
+Cambrian                -C<br/>
+Proterozoic             -P<br/>
+Neoproterozoic          N<br/>
+Mesoproterozoic         M<br/>
+Paleoproterozoic        L<br/>
+Archean                 A<br/>
+<br/>
+2. r = gross rock descriptor.  A one letter code to reflect the broad lithological composition of the unit<br/><br/>
 
-2. r = gross rock descriptor.  A one letter code to reflect the broad lithological composition of the unit
+IGNEOUS                                           EXAMPLES<br/>
+g  felsic to intermediate intrusive               granite, granodiorite, tonalite, monzonite, diorite, syenite<br/>
+d  mafic intrusive                                gabbro, dolerite, norite<br/>
+f  felsic extrusive / high level intrusive        rhyolite, dacite, ignimbrite, pyroclastic rocks<br/>
+a  intermediate extrusive / high level intrusive  andesite, trachyte, latite, pyroclastic rocks<br/>
+b  mafic extrusive / high level intrusive         basalt, scoria, shoshonite, pyroclastic rocks<br/>
+u  ultramafic undivided (intrusive & extrusive)   komatiite, high Mg basalt, pyroxenite, dunite, wehrlite<br/>
+k  alkaline ultramafic                            kimberlite, lamprophyre, carbonatite<br/><br/>
 
-IGNEOUS                                           EXAMPLES
-g  felsic to intermediate intrusive               granite, granodiorite, tonalite, monzonite, diorite, syenite
-d  mafic intrusive                                gabbro, dolerite, norite
-f  felsic extrusive / high level intrusive        rhyolite, dacite, ignimbrite, pyroclastic rocks
-a  intermediate extrusive / high level intrusive  andesite, trachyte, latite, pyroclastic rocks
-b  mafic extrusive / high level intrusive         basalt, scoria, shoshonite, pyroclastic rocks
-u  ultramafic undivided (intrusive & extrusive)   komatiite, high Mg basalt, pyroxenite, dunite, wehrlite
-k  alkaline ultramafic                            kimberlite, lamprophyre, carbonatite
+SEDIMENTARY<br/>
+s  siliciclastic/undifferentiated sediment        shale, siltstone, sandstone, conglomerate, mudstone<br/>
+j  volcanogenic sediment                          epiclastic sediments and breccias, greywacke, arkose<br/>
+l  carbonate sediment                             limestone, marl, dolomite<br/>
+c  non-carbonate chemical sediment                chert, evaporite, phosphorite, BIF<br/>
+o  organic-rich rock                              coal, amber, oil shale<br/><br/>
 
-SEDIMENTARY
-s  siliciclastic/undifferentiated sediment        shale, siltstone, sandstone, conglomerate, mudstone
-j  volcanogenic sediment                          epiclastic sediments and breccias, greywacke, arkose
-l  carbonate sediment                             limestone, marl, dolomite
-c  non-carbonate chemical sediment                chert, evaporite, phosphorite, BIF
-o  organic-rich rock                              coal, amber, oil shale
+MIXED SEDIMENTARY & IGNEOUS<br/>
+v  felsic & mafic volcanics<br/>
+i  felsic & mafic intrusives<br/> 
+w  volcanics & sediments<br/><br/>
 
-MIXED SEDIMENTARY & IGNEOUS
-v  felsic & mafic volcanics  
-i  felsic & mafic intrusives  
-w  volcanics & sediments  
-
-METAMORPHIC
-y  low-medium grade meta clastic sediment             slate, phyllite, schist, quartzite
-t  low-medium grade metabasite                  mafic schist, greenstone, amphibolite
-r  low-medium grade metafelsite                 rhyolitic schist, meta-andesite
-m  calc-silicate and marble                     meta carbonates and calcareous sediments
-n  high grade metamorphic rock                  gneiss, granulite, migmatite
-p  high-P metamorphic rock                      eclogite, blueschist
-h  contact metamorphic rock                     hornfels, spotted slate
+METAMORPHIC<br/>
+y  low-medium grade meta clastic sediment             slate, phyllite, schist, quartzite<br/>
+t  low-medium grade metabasite                  mafic schist, greenstone, amphibolite<br/>
+r  low-medium grade metafelsite                 rhyolitic schist, meta-andesite<br/>
+m  calc-silicate and marble                     meta carbonates and calcareous sediments<br/>
+n  high grade metamorphic rock                  gneiss, granulite, migmatite<br/>
+p  high-P metamorphic rock                      eclogite, blueschist<br/>
+h  contact metamorphic rock                     hornfels, spotted slate<br/>
 e  metamorphosed ultramafic rocks               serpentinite, talc schist, chlorite schist (no feldspars), tremolite schist, ultr
-amafic amphibolite
+amafic amphibolite<br/><br/>
 
-OTHER
-z  fault / shear rock                           mylonite, fault breccia, cataclasite, gouge
-q  vein                                         quartz vein, carbonate vein
-x                                               complex, melange, undivided, unknown
+OTHER<br/>
+z  fault / shear rock                           mylonite, fault breccia, cataclasite, gouge<br/>
+q  vein                                         quartz vein, carbonate vein<br/>
+x                                               complex, melange, undivided, unknown<br/><br/>
 
 3.   xy = One or two letters to reflect the stratigraphic name of a unit.  Where practical, these letters reflect stratigraphic g
-rouping or hierarchy.  For instance, formations within a named group should have letter symbols reflecting their parent group.
+rouping or hierarchy.  For instance, formations within a named group should have letter symbols reflecting their parent group.<br/><br/>
 
-eg:   Tomkinson Creek Group -  Lsk
+eg:   Tomkinson Creek Group -  Lsk<br/>
            Bootu Formation  -  Lskb', 
 true,
 'Surface Geology of Australia', 
@@ -151,7 +151,7 @@ true,
 '', 
 '', 
 'http://www.ga.gov.au/', 
-'Lithology',
+'Substrate',
 '', 
 '923', 
 '2010-01', 
@@ -161,14 +161,14 @@ NULL,
 NULL, 
 2,
 'http://creativecommons.org/licenses/by/2.5/au/deed.en',
-'This Geoscience Australia Data is available under the Creative Commons Attribution 2.5 Australia Licence
+'This Geoscience Australia Data is available under the Creative Commons Attribution 2.5 Australia Licence<br/><br/>
 
-You are free to:
-    - Share   -   to copy, adapt, distribute and transmit the work
-    - Remix   -   to adapt the work
+You are free to:<br/>
+    - Share   -   to copy, adapt, distribute and transmit the work<br/>
+    - Remix   -   to adapt the work<br/><br/>
 
-Under the following conditions:
--       Attribution - You must attribute the work as follows:
+Under the following conditions:<br/>
+-       Attribution - You must attribute the work as follows:<br/><br/>
 
 Copyright Commonwealth of Australia (Geoscience Australia) 2009 ',
 'http://www.ga.gov.au'
