aeb504b43dbd9748ef3e4ca4bf2905489ab32351
gavin.jackson@gmail.com <gavin.jackson@gmail.com>
2011-09-01 04:53:26 +0000

    Re-enabled some more layers on dev
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk@1267 73def43e-0e3b-11df-a5af-b38b39d06e06

--- database/update_layers_prod.sh
+++ database/update_layers_prod.sh
@@ -37,3 +37,4 @@ $PSQL_CMD -U postgres spatialdb -f ./layers_prod_updates/019_update_keywords_190
 $PSQL_CMD -U postgres spatialdb -f ./layers_prod_updates/020_gbr_keywords_190811.sql
 #$PSQL_CMD -U postgres spatialdb -f ./layers_prod_updates/021_aspa_asma_aat_290811.sql
 $PSQL_CMD -U postgres spatialdb -f ./layers_prod_updates/022_fire_enable_010911.sql
+#$PSQL_CMD -U postgres spatialdb -f ./layers_prod_updates/023_enable_layers_010911.sql 
