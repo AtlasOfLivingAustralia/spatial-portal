180200f68bfe871e454b12f6dd737bf599999b01
adam-collins <adam.collins832@gmail.com>
2012-04-26 04:37:05 +0000

    alaspatial - Update job query.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/alaspatial@1804 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/spatial/web/services/JobsController.java
+++ src/main/java/org/ala/spatial/web/services/JobsController.java
@@ -5,106 +5,51 @@
 
 package org.ala.spatial.web.services;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.ala.spatial.util.AnalysisQueue;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 /**
  *
  * @author Adam
  */
 @Controller
-@RequestMapping("/ws/jobs")
 public class JobsController {
+    static HashMap<String, Integer> lastLogPos = new HashMap<String, Integer>();
 
-    @RequestMapping(value = "/state", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/job", method = RequestMethod.GET)
     public
     @ResponseBody
-    String state(HttpServletRequest req) {
-        try {
-            String pid = req.getParameter("pid");
-
-            String s = AnalysisQueue.getState(pid);
-            if(s != null) return s;
-
+    Map job(@RequestParam(value = "pid", required = true, defaultValue = "") String pid) {
+        Map m = new HashMap<String, String>();
+        m.put("state", "job does not exist");
+        try {            
+            String state = AnalysisQueue.getState(pid);
+            if(state != null) {
+                m.put("state", state);
+                m.put("message", AnalysisQueue.getMessage(pid));
+                m.put("progress", AnalysisQueue.getProgress(pid));
+                m.put("status", AnalysisQueue.getStatus(pid));
+                
+                Integer pos = lastLogPos.containsKey(pid)?lastLogPos.get(pid): 0;
+                String log = AnalysisQueue.getLog(pid);
+                if(log == null) log = "";
+                m.put("log", log.substring(pos));
+                lastLogPos.put(pid,log.length());
+            }            
         } catch (Exception e){
             e.printStackTrace();
-        }
+        }        
 
-        return "job does not exist";
+        return m;
     }
 
-    @RequestMapping(value = "/message", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/listwaiting", method = RequestMethod.GET)
     public
     @ResponseBody
-    String message(HttpServletRequest req) {
-        try {
-            String pid = req.getParameter("pid");
-
-            String s = AnalysisQueue.getMessage(pid);
-            if(s != null) return s;
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return "job does not exist";
-    }
-
-    @RequestMapping(value = "/status", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String status(HttpServletRequest req) {
-        try {     
-            String pid = req.getParameter("pid");
-
-            String s =  AnalysisQueue.getStatus(pid);
-            if(s != null) return s;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return "job does not exist";
-    }
-
-    @RequestMapping(value = "/progress", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String progress(HttpServletRequest req) {
-        try {
-            String pid = req.getParameter("pid");
-
-            String s = AnalysisQueue.getProgress(pid);
-            if(s != null) return s;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return "job does not exist";
-    }
-
-    @RequestMapping(value = "/log", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String log(HttpServletRequest req) {
-        try {
-            String pid = req.getParameter("pid");
-
-            String s = AnalysisQueue.getLog(pid);
-            if(s != null) return s;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return "job does not exist";
-    }
-
-    @RequestMapping(value = "/listwaiting", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String listWaiting(HttpServletRequest req) {
+    String listWaiting() {
         try {
             String s = AnalysisQueue.listWaiting();
             if(s != null) return s;
@@ -116,11 +61,10 @@ public class JobsController {
         return "job does not exist";
     }
 
-
-    @RequestMapping(value = "/listrunning", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/listrunning", method = RequestMethod.GET)
     public
     @ResponseBody
-    String listRunning(HttpServletRequest req) {
+    String listRunning() {
         try {
             String s = AnalysisQueue.listRunning();
             if(s != null) return s;
@@ -132,10 +76,10 @@ public class JobsController {
         return "job does not exist";
     }
 
-    @RequestMapping(value = "/listfinished", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/listfinished", method = RequestMethod.GET)
     public
     @ResponseBody
-    String listFinished(HttpServletRequest req) {
+    String listFinished() {
         try {
             String s = AnalysisQueue.listFinished();
             if(s != null) return s;
@@ -147,12 +91,11 @@ public class JobsController {
         return "job does not exist";
     }
 
-    @RequestMapping(value = "/cancel", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/cancel", method = RequestMethod.GET)
     public
     @ResponseBody
-    String cancel(HttpServletRequest req) {
+    String cancel(@RequestParam(value = "pid", required = true, defaultValue = "") String pid) {
         try {
-            String pid = req.getParameter("pid");
 
             AnalysisQueue.cancelJob(pid);
             return "";
@@ -163,13 +106,11 @@ public class JobsController {
         return "job does not exist";
     }
 
-    @RequestMapping(value = "/inputs", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/inputs", method = RequestMethod.GET)
     public
     @ResponseBody
-    String inputs(HttpServletRequest req) {
+    String inputs(@RequestParam(value = "pid", required = true, defaultValue = "") String pid) {
         try {
-            String pid = req.getParameter("pid");
-
             String s = AnalysisQueue.getInputs(pid);
             return s;
         } catch (Exception e){
@@ -179,13 +120,11 @@ public class JobsController {
         return "job does not exist";
     }
 
-    @RequestMapping(value = "/image", method = RequestMethod.GET)
+    @RequestMapping(value = "/ws/jobs/image", method = RequestMethod.GET)
     public
     @ResponseBody
-    String image(HttpServletRequest req) {
+    String image(@RequestParam(value = "pid", required = true, defaultValue = "") String pid) {
         try {
-            String pid = req.getParameter("pid");
-
             String s = AnalysisQueue.getImage(pid);
             return s;
         } catch (Exception e){
@@ -194,25 +133,4 @@ public class JobsController {
 
         return "job does not exist";
     }
-
-    @RequestMapping(value = "/copy", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    String copy(HttpServletRequest req) {
-        try {
-            String pid = req.getParameter("pid");
-
-            String s = AnalysisQueue.copy(pid);
-
-            if(s == null) s = "";
-            
-            return s;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return "job does not exist";
-    }
 }
-
-
