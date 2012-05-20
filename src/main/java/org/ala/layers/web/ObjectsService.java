c4006d3b5ad9010fe56ef63e190c4b531d8309e6
adam-collins <adam.collins832@gmail.com>
2012-05-20 01:50:24 +0000

    layers-service - Add objects in area service. - Update pom.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layers-service@1954 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/layers/web/ObjectsService.java
+++ src/main/java/org/ala/layers/web/ObjectsService.java
@@ -12,14 +12,18 @@
  *  implied. See the License for the specific language governing
  *  rights and limitations under the License.
  ***************************************************************************/
-
 package org.ala.layers.web;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.ala.layers.dao.ObjectDAO;
 import org.ala.layers.dto.Objects;
+import org.ala.layers.util.LayerFilter;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,32 +38,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class ObjectsService {
+
     /**
      * Log4j instance
      */
     protected Logger logger = Logger.getLogger(this.getClass());
-
-    @Resource(name="objectDao")
+    @Resource(name = "objectDao")
     private ObjectDAO objectDao;
 
-     /**This method returns all objects associated with a field
+    /**This method returns all objects associated with a field
      * 
      * @param id
      * @param req
      * @return 
      */
     @RequestMapping(value = "/objects/{id}", method = RequestMethod.GET)
-    public @ResponseBody List<Objects> fieldObjects(@PathVariable("id") String id, HttpServletRequest req) {
+    public 
+    @ResponseBody
+    List<Objects> fieldObjects(@PathVariable("id") String id, HttpServletRequest req) {
         return objectDao.getObjectsById(id);
     }
-    
+
     /**This method returns a single object, provided a UUID
      * 
      * @param req
      * @return 
      */
     @RequestMapping(value = "/object/{pid}", method = RequestMethod.GET)
-    public @ResponseBody Objects fieldObject(@PathVariable("pid") String pid, HttpServletRequest req) {
+    public 
+    @ResponseBody
+    Objects fieldObject(@PathVariable("pid") String pid, HttpServletRequest req) {
 //        String query = "SELECT pid, id, name, \"desc\" FROM objects WHERE pid='" + pid + "';";
 //        ResultSet r = DBConnection.query(query);
 //        return Utils.resultSetToJSON(r);
@@ -73,10 +81,63 @@ public class ObjectsService {
      * @return
      */
     @RequestMapping(value = "/objects/{id}/{lat}/{lng:.+}", method = RequestMethod.GET)
-    public @ResponseBody List<Objects> fieldObjects(@PathVariable("id") String id,
-            @PathVariable("lat") Double lat,@PathVariable("lng") Double lng,
+    public 
+    @ResponseBody
+    List<Objects> fieldObjects(@PathVariable("id") String id,
+            @PathVariable("lat") Double lat, @PathVariable("lng") Double lng,
             @RequestParam(value = "limit", required = false, defaultValue = "40") Integer limit,
-            HttpServletRequest req) {        
+            HttpServletRequest req) {
         return objectDao.getNearestObjectByIdAndLocation(id, limit, lng, lat);
+    }
+
+    /**This method returns all objects associated with a field
+     *
+     * @param id
+     * @param req
+     * @return
+     */
+    @RequestMapping(value = "/objects/{id}", method = { RequestMethod.GET, RequestMethod.POST })
+    public 
+    @ResponseBody
+    List<Objects> fieldObjects(@PathVariable("id") String id,
+            @RequestParam(value = "limit", required = false, defaultValue = "40") Integer limit,
+            HttpServletRequest req) throws UnsupportedEncodingException {
+        String wkt = URLDecoder.decode(req.getParameter("wkt"),"UTF-8");
+        if (wkt.toUpperCase().startsWith("ENVELOPE(")) {
+            //get results of each filter
+            LayerFilter[] filters = LayerFilter.parseLayerFilters(wkt);
+            List<List<Objects>> all = new ArrayList<List<Objects>>();
+            for (int i = 0; i < filters.length; i++) {
+                all.add(objectDao.getObjectsByIdAndIntersection(id, limit, filters[i]));
+            }
+            //merge common entries only
+            HashMap<String, Integer> objectCounts = new HashMap<String, Integer>();
+            List<Objects> list = all.get(0);
+            for (int j = 0; j < list.size(); j++) {
+                objectCounts.put(list.get(j).getPid(), 1);
+            }
+            for (int i = 1; i < all.size(); i++) {
+                List<Objects> t = all.get(i);
+                for (int j = 0; j < t.size(); j++) {
+                    Integer v = objectCounts.get(t.get(j).getPid());
+                    if (v != null) {
+                        objectCounts.put(t.get(j).getPid(), v + 1);
+                    }
+                }
+            }
+            List<Objects> inAllGroups = new ArrayList<Objects>(list.size());
+            for (int j = 0; j < list.size(); j++) {
+                if (objectCounts.get(list.get(j).getPid()) == all.size()) {
+                    inAllGroups.add(list.get(j));
+                }
+            }
+
+            return inAllGroups;
+        } else if (wkt.toUpperCase().startsWith("OBJECT(")) {
+            String pid = wkt.substring("OBJECT(".length(), wkt.length() - 1);
+            return objectDao.getObjectsByIdAndIntersection(id, limit, pid);
+        } else {
+            return objectDao.getObjectsByIdAndArea(id, limit, wkt);
+        }
     }
 }
