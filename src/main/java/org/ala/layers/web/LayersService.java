a5d5c0bcf0d70f1d63ad67b75958b6bbf8a9aa74
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com>
2012-11-26 07:58:00 +0000

    Further work to provide AANDS format xml layers list
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layers-service@2271 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/layers/web/LayersService.java
+++ src/main/java/org/ala/layers/web/LayersService.java
@@ -15,11 +15,18 @@
 
 package org.ala.layers.web;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.URI;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.ala.layers.dao.LayerDAO;
 import org.ala.layers.dto.Layer;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +37,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- *
+ * 
  * @author jac24n
  */
 @Controller
@@ -39,71 +46,153 @@ public class LayersService {
      * Log4j instance
      */
     protected Logger logger = Logger.getLogger(this.getClass());
-     
-    @Resource(name="layerDao")
+
+    @Resource(name = "layerDao")
     private LayerDAO layerDao;
 
-    /**This method returns all layers
+    /**
+     * This method returns all layers
      * 
      * @param req
-     * @return 
+     * @return
      */
     @RequestMapping(value = "/layers", method = RequestMethod.GET)
-    public @ResponseBody List<Layer> layerObjects(HttpServletRequest req) {
+    public @ResponseBody
+    List<Layer> layerObjects(HttpServletRequest req) {
         logger.info("Retriving enabled layers");
         return layerDao.getLayers();
     }
-    
-    /**This method returns a single layer, provided an id
+
+    /**
+     * This method returns a single layer, provided an id
      * 
      * @param uuid
      * @param req
-     * @return 
+     * @return
      */
     @RequestMapping(value = "/layer/{id}", method = RequestMethod.GET)
-    public @ResponseBody Layer layerObject(@PathVariable("id") String id, HttpServletRequest req) {
+    public @ResponseBody
+    Layer layerObject(@PathVariable("id") String id, HttpServletRequest req) {
         Layer l = null;
         try {
             l = layerDao.getLayerById(Integer.parseInt(id));
-        }catch(Exception e) {}
-        
-        if (l==null) {
+        } catch (Exception e) {
+        }
+
+        if (l == null) {
             l = layerDao.getLayerByName(id);
         }
         return l;
     }
-    
-    /**This method returns all layers
-     *
+
+    /**
+     * This method returns all layers
+     * 
      * @param req
      * @return
      */
     @RequestMapping(value = "/layers/search", method = RequestMethod.GET)
-    public @ResponseBody List<Layer> layerObjects(@RequestParam("q") String q, HttpServletRequest req) {
+    public @ResponseBody
+    List<Layer> layerObjects(@RequestParam("q") String q, HttpServletRequest req) {
         logger.info("search enabled layers for " + q);
-        return layerDao.getLayersByCriteria(q); 
+        return layerDao.getLayersByCriteria(q);
     }
 
     @RequestMapping(value = "/layers/grids", method = RequestMethod.GET)
-    public @ResponseBody List<Layer> gridsLayerObjects(HttpServletRequest req) {
-//        String query = "SELECT * FROM layers WHERE enabled='TRUE' and type='Environmental';";
-//        ResultSet r = DBConnection.query(query);
-//        return Utils.resultSetToJSON(r);
+    public @ResponseBody
+    List<Layer> gridsLayerObjects(HttpServletRequest req) {
+        // String query =
+        // "SELECT * FROM layers WHERE enabled='TRUE' and type='Environmental';";
+        // ResultSet r = DBConnection.query(query);
+        // return Utils.resultSetToJSON(r);
         return layerDao.getLayersByEnvironment();
     }
-        
+
     @RequestMapping(value = "/layers/shapes", method = RequestMethod.GET)
-    public @ResponseBody List<Layer> shapesLayerObjects(HttpServletRequest req) {
-//        String query = "SELECT * FROM layers WHERE enabled='TRUE' and type='Contextual';";
-//        ResultSet r = DBConnection.query(query);
-//        return Utils.resultSetToJSON(r);
+    public @ResponseBody
+    List<Layer> shapesLayerObjects(HttpServletRequest req) {
+        // String query =
+        // "SELECT * FROM layers WHERE enabled='TRUE' and type='Contextual';";
+        // ResultSet r = DBConnection.query(query);
+        // return Utils.resultSetToJSON(r);
         return layerDao.getLayersByContextual();
 
     }
-    
-    @RequestMapping(value = "/layers/aands", method = RequestMethod.GET)
-    public ModelAndView generatesAandsXml(HttpServletRequest req) {
-        return new ModelAndView("layers/aands", "layers", layerDao.getLayers());
+
+    /**
+     * Return layers list if RIF-CS XML format
+     * @param req
+     * @param res
+     * @throws Exception
+     */
+    @RequestMapping(value = "/layers/rif-cs", method = RequestMethod.GET)
+    public void layersRifCs(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        res.setContentType("text/xml");
+
+        // Build XML by hand here because JSP processing seems to omit CDATA sections from the output
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+        sb.append("<registryObjects xmlns=\"http://ands.org.au/standards/rif-cs/registryObjects\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://ands.org.au/standards/rif-cs/registryObjects http://services.ands.org.au/documentation/rifcs/schema/registryObjects.xsd\">");
+        for (Layer layer: layerDao.getLayers()) {
+            sb.append("<registryObject group=\"Atlas of Living Australia\">");
+            sb.append("<key>ala.org.au/uid_" + layer.getUid() + "</key>");
+            sb.append("<originatingSource><![CDATA[" + layer.getMetadatapath() + "]]></originatingSource>");
+            sb.append("<collection type=\"dataset\">");
+            sb.append("<identifier type=\"local\">ala.org.au/uid_" + layer.getUid() + "</identifier>");
+            sb.append("<name type=\"abbreviated\">");
+            sb.append("<namePart>" + layer.getName() + "</namePart>");
+            sb.append("</name>");
+            sb.append("<name type=\"alternative\">");
+            sb.append("<namePart><![CDATA[" + layer.getDescription() + "]]></namePart>");
+            sb.append("</name>");
+            sb.append("<name type=\"primary\">");
+            sb.append("<namePart><![CDATA[" + layer.getDescription() + "]]></namePart>");
+            sb.append("</name>");
+            sb.append("<location>");
+            sb.append("<address>");
+            sb.append("<electronic type=\"url\">");
+            sb.append("<value>http://spatial.ala.org.au/layers</value>");
+            sb.append("</electronic>");
+            sb.append("</address>");
+            sb.append("</location>");
+            sb.append("<relatedObject>");
+            sb.append("<key>Contributor:Atlas of Living Australia</key>");
+            sb.append("<relation type=\"hasCollector\" />");
+            sb.append("</relatedObject>");
+            sb.append("<subject type=\"anzsrc-for\">0502</subject>");
+            sb.append("<subject type=\"local\">" + layer.getClassification1() + "</subject>");
+            if (!StringUtils.isEmpty(layer.getClassification2())) {
+                sb.append("<subject type=\"local\">" + layer.getClassification2() + "</subject>");
+            }
+            sb.append("<description type=\"full\"><![CDATA[" + layer.getNotes() + "]]></description>");
+            sb.append("<relatedInfo type=\"website\">");
+            sb.append("<identifier type=\"uri\"><![CDATA[" + layer.getMetadatapath() + "]]></identifier>");
+            sb.append("<title>Further metadata</title>");
+            sb.append("</relatedInfo>");
+            sb.append("<relatedInfo type=\"website\">");
+            sb.append("<identifier type=\"uri\"><![CDATA[" + layer.getSource_link() + "]]></identifier>");
+            sb.append("<title>Original source of this data</title>");
+            sb.append("</relatedInfo>");
+            sb.append("<rights>");
+            sb.append("<licence ");
+            if (!StringUtils.isEmpty(layer.getLicence_link())) {
+                sb.append("rightsUri=\"" + StringEscapeUtils.escapeXml(layer.getLicence_link()) + "\">");
+            } else {
+                sb.append(">");
+            }
+            sb.append("<![CDATA[" + layer.getLicence_notes() + "]]></licence>");
+            sb.append("</rights>");
+            sb.append("<coverage>");
+            sb.append("<spatial type=\"iso19139dcmiBox\">northlimit=" + layer.getMaxlatitude() + "; southlimit=" + layer.getMinlatitude() + "; westlimit=" + layer.getMinlongitude() + "; eastLimit=" + layer.getMaxlongitude()+ "; projection=WGS84</spatial>");
+            sb.append("</coverage>");
+            sb.append("</collection>");
+            sb.append("</registryObject>");
+        }
+        sb.append("</registryObjects>");
+        
+        res.getWriter().append(sb.toString());
+        res.getWriter().flush();
+        res.getWriter().close();
     }
-    
+
 }
