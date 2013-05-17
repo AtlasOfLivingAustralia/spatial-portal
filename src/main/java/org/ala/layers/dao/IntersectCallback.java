f0eff0c9b62cfae7b7e9e8982f7fb51ac908b99c
djtfmartin <djtfmartin@gmail.com>
2013-05-17 06:55:41 +0000

    refactorings to support sandbox monitoring of processes
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layers-store@2355 73def43e-0e3b-11df-a5af-b38b39d06e06

---
+++ src/main/java/org/ala/layers/dao/IntersectCallback.java
@@ -0,0 +1,10 @@
+package org.ala.layers.dao;
+
+import org.ala.layers.dto.IntersectionFile;
+
+public interface IntersectCallback {
+    public void setLayersToSample(IntersectionFile[] layersToSample);
+    public void setCurrentLayer(IntersectionFile layer);
+    public void setCurrentLayerIdx(Integer layerIdx);
+    public void progressMessage(String message);
+}
