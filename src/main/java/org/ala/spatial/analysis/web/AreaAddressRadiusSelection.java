c53d64a284ff381714f190ca8a8532aa364dbcf1
djtfmartin <djtfmartin@gmail.com>
2013-08-13 10:18:36 +0000

    fix to use latest geogoogle api
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/webportal@2409 73def43e-0e3b-11df-a5af-b38b39d06e06

--- src/main/java/org/ala/spatial/analysis/web/AreaAddressRadiusSelection.java
+++ src/main/java/org/ala/spatial/analysis/web/AreaAddressRadiusSelection.java
@@ -9,9 +9,6 @@ import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.io.WKTWriter;
-import geo.google.GeoAddressStandardizer;
-import geo.google.datamodel.GeoAddress;
-import geo.google.datamodel.GeoCoordinate;
 
 import java.net.URL;
 import java.net.URLEncoder;
